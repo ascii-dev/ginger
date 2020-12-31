70195a83b24f1c25e512d1ca25c8495d95144bfd
Samuel Raphael <sammysgame.dev@gmail.com>
2020-12-31 04:40:31 +0100

    single article page (#4)

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from articles.views.all_articles import AllArticlesView
+from articles.views.single_article import ArticleView
 
 
 urlpatterns = [
     path('', AllArticlesView.as_view(), name='all-articles'),
+    path('articles/<str:article_id>/', ArticleView.as_view(), name='single-article'),
 ]
