553f1fce62fb5fed5f1353f2e18da62edf0e839e
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2021-01-02 20:06:57 +0100

    distinguish between favorited and non favorited

--- articles/views/single_article.py
+++ articles/views/single_article.py
@@ -3,6 +3,8 @@ from django.http import Http404
 from django.views import View
 from django.shortcuts import render
 
+from favorites.models import Favorite
+
 
 class ArticleView(View):
     template_name = 'articles/single.html'
@@ -17,5 +19,14 @@ class ArticleView(View):
         if not article:
             raise Http404("Article not found!")
 
+        context = dict(article=article[0])
+
+        if request.user.is_authenticated:
+            # TODO: there possibly is a better way to do this
+            try:
+                context['favorite'] = Favorite.objects.get(article_id=article_id, user=request.user)
+            except Favorite.DoesNotExist:
+                context['favorite'] = None
+
         # The api returns a list of a single element
-        return render(request, self.template_name, {'article': article[0]})
+        return render(request, self.template_name, context)
